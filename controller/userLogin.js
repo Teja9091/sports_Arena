29aa97768c0906558971140efb4b8661b591fa88
tejacc <teja.chalasani@cognitiveclouds.com>
2020-02-10 14:10:25 +0530

    jwt token generated succesfully

--- controller/userLogin.js
+++ controller/userLogin.js
@@ -1,8 +1,16 @@
 const express = require('express');
+const jwt = require('jsonwebtoken');
 const mongoose = require('mongoose');
 const router = express.Router();
 const uLogin = require('../service/login');
+const user = require('../models/user')
 
 router.post('/',uLogin);
+/* (req,res) => {
+    jwt.sign({user},'privatekey',(err,token) => {
+        console.log(token);
+    });
+});
+*/
 
 module.exports = router;
--- service/login.js
+++ service/login.js
@@ -1,4 +1,5 @@
 const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
 const  User = require('../models/user');
 
 const auth = async (req,res) => {
@@ -9,7 +10,13 @@ const auth = async (req,res) => {
     const validPassword = await bcrypt.compare(req.body.pwd, user.pwd);
     if(!validPassword) return res.status(400).send('Invalid email or password');
 
+    jwt.sign({user},'privatekey',(err,token) => {
+        console.log(token);
+        res.send(token);
+    })
     res.send('Successfully logged-in');
+
+    
 };
 
 module.exports = auth;
