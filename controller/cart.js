8c04bc470f400aed55bb2cb9d1338b46fbe3c3e5
tejacc <teja.chalasani@cognitiveclouds.com>
2020-03-10 15:19:32 +0530

    few changes

--- controller/cart.js
+++ controller/cart.js
@@ -2,28 +2,30 @@ const express = require('express');
 const Cart = require('../models/cart')
 const router = express.Router();
 
-router.post('/add',async (req,res) => {
-    if(Cart.productId) {
-        for(let i=1; i < Cart.productId.length; i++) {
-            Cart.productId = Cart.productId.length
-        }
-    } else {
-        try {
-            let cart = new Cart({
-                userId: req.body.userId,
-                productId: req.body.productId,
-                quantity: req.body.quantity
-            });
-                await cart.save();
-                res.status(201).send(cart);
-        } catch (err) {
-            res.status(400).json({Error:"Error while updating cart to product"});
-        }
+router.put('/:userId&:productId',async (req,res) => {
+    try {
+        const product = await Cart.findOneAndUpdate({userId:req.params.userId,productId:req.params.productId},req.body);
+        res.status(201).send(product);
+    } catch (err) {
+        res.status(500).json({Error:"Update failed"});
     }
 });
-router.get('/',async (req,res) => {
+router.post('/add',async (req,res) => {
     try {
-        const products = await Cart.findOne({userId:req.query.userId})
+        let cart = new Cart({
+            userId: req.body.userId,
+            productId: req.body.productId,
+            quantity: req.body.quantity
+        });
+            await cart.save();
+            res.status(201).send(cart);
+    } catch (err) {
+        res.status(400).json({Error:"Error while updating cart to product"});
+    }
+});
+router.get('/:userId',async (req,res) => {
+    try {
+        const products = await Cart.findOne({userId:req.params.userId})
         .populate('product user');
          res.status(200).send(products);
     } catch (err) {
